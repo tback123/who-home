7c78fd55fe69d7833b268fc460942bc61f5363d8
tback123 <tomkillingback@gmail.com>
2020-02-07 22:05:34 +1100

    Add login form state

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Grid, TextField } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
   return (
-    <>
-      <Grid container direction="column" spacing={1} alignContent="center">
-        <Grid item>
-          <TextField label="Username" variant="outlined" />
-        </Grid>
-        <Grid item>
-          <TextField label="Password" variant="outlined" />
-        </Grid>
-        <Grid item>
-          <Grid container direction="row-reverse" alignContent="flex-end">
-            <Grid item>
-              <Link to="/home" style={{ textDecoration: 'none' }}>
-                <Button variant="contained" color="primary">
-                  Login
-                </Button>
-              </Link>
-            </Grid>
+    <Grid container direction="column" spacing={1} alignContent="center">
+      <Grid item>
+        <TextField label="Username" variant="outlined" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+      </Grid>
+      <Grid item>
+        <TextField label="Password" variant="outlined" value={password} onChange={e => setPassword(e.target.value)}/>
+      </Grid>
+      <Grid item>
+        <Grid container direction="row-reverse" alignContent="flex-end">
+          <Grid item>
+            <Link to="/home" style={{ textDecoration: "none" }}>
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
+            </Link>
           </Grid>
         </Grid>
       </Grid>
-    </>
+    </Grid>
   );
 };
 
