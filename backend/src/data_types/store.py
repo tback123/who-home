38f804d1cf80fd457e58aa99830bb0754665e68b
Tom Killingback <tomkillingback@gmail.com>
2020-05-18 20:29:16 +1000

    Added mongoDB Wrapper
    Co-authored-by: Squar-3D <george.kamateros@hotmail.com>

--- backend/src/data_types/store.py
+++ backend/src/data_types/store.py
@@ -4,33 +4,54 @@
     Syncs it every second to a file
 '''
 
+from secrets import MONGODB_URL, MONGODB_NAME
+import pymongo
+
 class Store:
     ''' Defines the Store Class '''
-    def __init__(self):
-        self.__users = []
-        self.__channels = []
 
-    def get_all_users(self):
-        ''' Returns list of all users'''
+    def __init__(self):
+        __db = pymongo.MongoClient(MONGODB_URL)[MONGODB_NAME]
+        self.__users = __db["users"]
+        self.__households = __db["households"]
+
+    def get_user_collection(self):
+        ''' Returns mongoDB user collection'''
         return self.__users
 
-    def add_user(self, new_user):
-        ''' Adds User Object to list of users'''
-        self.__users.append(new_user)
+    def get_household_collection(self):
+        ''' Returns mongoDB household collection'''
+        return self.__households
 
-    def remove_user_by_id(self, user_id):
-        ''' Removes user from list of users by id '''
-        user_to_remove = next((user for user in self.__users if user.get_u_id() == user_id), None)
-        self.__users.remove(user_to_remove)
+    def get_users_list(self):
+        ''' Returns list of dictionaries of users '''
+        return_lst = []
+        for usr in self.__users.find():
+            return_lst.append(usr)
 
-    def get_user_by_id(self, user_id):
-        ''' Returns a User object given a u_id '''
-        return next((user for user in self.__users if user.get_u_id() == user_id), None)
+        return return_lst
 
-    def reset_store(self):
-        ''' Clears store '''
-        self.__users = []
+    def get_household_list(self):
+        ''' Returns list of dictionaries of household '''
+        return_lst = []
+        for house in self.__households.find():
+            return_lst.append(house)
 
+        return return_lst
+
+    def update_one_user(self, u_id, new_attribute_dict):
+        '''
+            Given a user_id, will update that specific user's attribute, 
+            as assigned in the second argument
+        '''
+        self.__users.update_one({u_id: u_id}, new_attribute_dict)
+
+    def update_one_household(self, h_id, new_attribute_dict):
+        '''
+            Given a user_id, will update that specific user's attribute, 
+            as assigned in the second argument
+        '''
+        self.__households.update_one({h_id: h_id}, new_attribute_dict)
 
 STORE = Store()
 
