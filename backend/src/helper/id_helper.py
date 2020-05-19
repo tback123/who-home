ec6bf642703dfaa736de5ff9d67f76195d2565ef
Tom Killingback <tomkillingback@gmail.com>
2020-05-19 13:18:18 +1000

    added h_id to helper

--- backend/src/helper/id_helper.py
+++ backend/src/helper/id_helper.py
@@ -18,6 +18,19 @@ def is_u_id_in_use(u_id):
 
     return False
 
+def is_h_id_in_use(h_id):
+    '''
+        Checks if the household id is in use
+        Returns True or False
+    '''
+
+    for household in get_store().get_households_list():
+        if h_id == household['h_id']:
+            return True
+
+    return False
+
+
 def new_u_id():
     '''
         Returns a new unique u_id int.
@@ -28,3 +41,15 @@ def new_u_id():
         u_id = int(datetime.now().timestamp() + random.randint(0, 10000))
 
     return u_id
+
+def new_h_id():
+    '''
+        Returns a new unique h_id int.
+        This h_id will never have been previosuly used
+    '''
+    h_id = None
+    while is_h_id_in_use(h_id) or h_id is None:
+        h_id = int(datetime.now().timestamp() + random.randint(0, 10000))
+
+    return h_id
+
